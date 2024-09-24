de2f8176d2fab38a049163659995ed76467c47c2
safrotm <tygropkjgdxhcx@gmail.com>
2024-09-25 01:32:44 +0300

    Update اخرج.js

--- plugins/اخرج.js
+++ plugins/اخرج.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, command }) => {
 let id = text ? text : m.chat  
-await conn.reply(id, '*لقد قضيت معكم وقتاً رائعاً الى اللقاء ⁦^⁠_⁠^⁩*') 
+await conn.reply(id, '*انا خارج يولاد المتناكه🐤*') 
 await conn.groupLeave(id)}
 handler.command = /^(اخرج|اطلع|غادر|خروج)$/i
 handler.group = true

00cbf0f292654f380dcabe5f40efb0997d8415fe
safrotm <tygropkjgdxhcx@gmail.com>
2024-09-25 04:01:49 +0300

    Update ستاكت.js

--- plugins/ستاكت.js
+++ plugins/ستاكت.js
@@ -14,7 +14,7 @@ let text
     const mentionRegex = new RegExp(`@${who.split('@')[0].replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\s*`, 'g');
     const mishi = text.replace(mentionRegex, '');
    if (mishi.length > 30) return m.reply('*[❗️] عدد الحروف كتيره الحد الأدنى 30*');
-    const pp = await conn.profilePictureUrl(who).catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    const pp = await conn.profilePictureUrl(who).catch((_) => 'https://i.ibb.co/52H5GwS/file.jpg')
     const nombre = await conn.getName(who)
     const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": `${who?.name || nombre}`, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
     const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
